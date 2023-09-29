a4951d26639afc41f2bedf22758e8668fef0ee5a
thisisjackii <jaxsix06@gmail.com>
2023-09-29 12:53:16 +0700

    Insert Routes in Main file

--- api/app.js
+++ api/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const jadwalKelasRoutes = require('./routes/jadwalKelasRoutes');
+const dataDosenRoutes = require('./routes/dataDosenRoutes');
+const dataDosenWalidRoutes = require('./routes/dataDosenWalidRoutes');
+const dataJamPelajaranRoutes = require('./routes/dataJamPelajaranRoutes');
+const dataKelasRoutes = require('./routes/dataKelasRoutes');
+const dataMahasiswaRoutes = require('./routes/dataMahasiswaRoutes');
+const dataMataKuliahRoutes = require('./routes/dataMataKuliahRoutes');
 const dataPengajuanRoutes = require('./routes/dataPengajuanRoutes');
+const jadwalKelasRoutes = require('./routes/jadwalKelasRoutes');
 
 const app = express();
 app.use(bodyParser.json());
 
 // Use your routes here
-app.use('/class-schedules', classScheduleRoutes);
+app.use('/class-schedules', jadwalKelasRoutes);
 app.use('/leave-requests', dataPengajuanRoutes);
 
 const PORT = process.env.PORT || 3000;
