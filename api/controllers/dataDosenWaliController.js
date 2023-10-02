99e580f5a1b7a499b551ffe926536f99c789faf6
thisisjackii <jaxsix06@gmail.com>
2023-10-02 17:26:20 +0700

    Try login functionality

--- api/controllers/dataDosenWaliController.js
+++ api/controllers/dataDosenWaliController.js
@@ -1,6 +1,8 @@
 // const Data_Dosen_Wali = require('../models/models/dataDosenWali');
 const path = require('path');
 const basename = path.basename(__filename);
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const {mainModel} = require('../common/models');
 const Data_Dosen_Wali = new mainModel("Data_Dosen_Wali");
 
@@ -17,5 +19,20 @@ exports.getAllAdviserLecturers = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+exports.loginAdviserLecturer = async (req, res) => {
+  try {
+    const {Email_Dosen, Password} = req.body;
+    const dosenWali = await Data_Dosen_Wali.get(Email_Dosen);
+    const passwordMatch = await bcrypt.compare(Password, dosenWali.Password);
+    if (!dosenWali || !passwordMatch) {
+      return res.status(401).json({message: 'Invalid username or password'});
+    }
+    const token = jwt.sign({userId: dosenWali.Email_Dosen}, 'secretKey', { expiresIn: '1h' });
+    res.status(200).json({ token });
+  } catch (error) {
+    console.error(error);
   }
 };
