74ded642df7f5769f093be29f6481192286e5c7d
thisisjackii <jaxsix06@gmail.com>
2023-10-03 12:37:09 +0700

    Add register and login routes (WIP)

--- api/routes/dataDosenWaliRoutes.js
+++ api/routes/dataDosenWaliRoutes.js
@@ -5,4 +5,8 @@ const dataDosenWaliController = require('../controllers/dataDosenWaliController'
 // Get all adviser lecturers
 router.get('/', dataDosenWaliController.getAllAdviserLecturers);
 
+router.post('/register', dataDosenWaliController.registerAdviserLecturer);
+
+router.post('/login', dataDosenWaliController.loginAdviserLecturer);
+
 module.exports = router;
--- api/routes/dataMahasiswaRoutes.js
+++ api/routes/dataMahasiswaRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const dataMahasiswaController = require('../controllers/dataMahasiswaController');
+const mhsAuth = require('../middleware/mhsAuth')
 
 // Get all students
 router.get('/', dataMahasiswaController.getAllStudents);
+
+router.post('/register', mhsAuth.saveUser, dataMahasiswaController.registerStudent);
+
+router.post('/login', dataMahasiswaController.loginStudent);
 
 module.exports = router;
