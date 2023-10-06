3db478c99a19e98d642dc69fd15a662ab350d519
auliaziizah <auliaziizah@gmail.com>
2023-10-06 11:33:36 +0700

    route crud dosen

--- react/src/App.js
+++ react/src/App.js
@@ -18,10 +18,14 @@ const Register = React.lazy(() => import('./views/pages/register/Register'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 const FormPengajuan = React.lazy(() => import('./views/pages/formPengajuan/FormPengajuan'))
-const DetailDosen = React.lazy(()=> import('./views/pages/crudDosen/DetailDosen'))
 const LandingPage = React.lazy(()=> import('./views/pages/landingPage/LandingPage'))
 //CRUD Mahasiswa (TU)
 const EditMahasiswa = React.lazy(() => import('./views/pages/crudMahasiswa/EditMahasiswa'))
+//CRUD Dosen (TU)
+const CrudDosen = React.lazy(() => import('./views/pages/crudDosen/tabelDosen'))
+const DetailDosen = React.lazy(() => import('./views/pages/crudDosen/detailDosen'))
+const TambahDosen = React.lazy(()=> import('./views/pages/crudDosen/tambahDosen'));
+const EditDosen = React.lazy(() => import('./views/pages/crudDosen/editDosen'))
 class App extends Component {
   render() {
     return (
@@ -32,10 +36,13 @@ class App extends Component {
             <Route exact path="/register" name="Register Page" element={<Register />} />
             <Route exact path="/404" name="Page 404" element={<Page404 />} />
             <Route exact path="/500" name="Page 500" element={<Page500 />} />
-            <Route exact path="/detail-dosen/:key" name="DetailDosen" element={<DetailDosen />} />
             <Route exact path="/tu/mahasiswa/edit" name="EditMahasiswa" element={<EditMahasiswa />} />
             <Route exact path="/formPengajuan" name="form pengajuan" element={<FormPengajuan />} />
             <Route index name="landing page" element={<LandingPage />} />
+            <Route exact path="/dataDosen" name="TabelDosen" element={<CrudDosen />} />
+            <Route exact path="/detailDosen/:key" name="DetailDosen" element={<DetailDosen />} />
+            <Route exact path="/tambahDosen" name="TambahDosen" element={<TambahDosen />} />
+            <Route exact path="/editDosen" name="EditDosen" element={<EditDosen />} />
             <Route path="*" name="Home" element={<LayoutSLP />} />
           </Routes>
         </Suspense>
