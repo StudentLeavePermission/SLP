b43fa1cd7b15134f59b65076004171a0b5510451
mdifal <mdifa1565@gmail.com>
2023-10-18 10:21:45 +0700

    Push detail dan tabel mahasiswa

--- react/src/views/pages/crudMahasiswa/tabelMahasiswa.js
+++ react/src/views/pages/crudMahasiswa/tabelMahasiswa.js
@@ -1,151 +1,199 @@
-import React, { useState, useEffect, useRef } from 'react';
-import 'datatables.net-dt/css/jquery.dataTables.css'; // Import DataTables CSS
-import $ from 'jquery'; // Import jQuery
-import 'datatables.net'; // Import DataTables
-import './tabelMahasiswa'; // Import your CSS file
+import React, { useState, useEffect } from 'react';
+import './tabelMahasiswa.css'; // Import your CSS file
 import CIcon from '@coreui/icons-react';
-import { cilInfo, cilTrash, cilPencil } from '@coreui/icons';
+import { cilInfo, cilTrash, cilPencil, cilSearch, cilArrowTop, cilArrowBottom } from '@coreui/icons';
 import { CButton } from '@coreui/react';
-import avatar1 from 'src/assets/images/avatars/1.jpg';
-import avatar2 from 'src/assets/images/avatars/2.jpg';
+import axios from "axios";
 
 function TabelCRUD() {
-  const tableRef = useRef(null);
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(5);
+  const [searchText, setSearchText] = useState('');
+  const [sortBy, setSortBy] = useState('Nama');
+  const [sortOrder, setSortOrder] = useState('asc');
 
   useEffect(() => {
-    // Mengatur opsi bahasa DataTables
-    $.extend($.fn.dataTable.defaults, {
-      language: {
-        paginate: {
-          previous: '<', // Mengubah "Previous" menjadi tanda "<"
-          next: '>', // Mengubah "Next" menjadi tanda ">"
-        },
-      },
-    });
-
-    // Inisialisasi DataTables pada tabel menggunakan ref
-    const dataTable = $(tableRef.current).DataTable({
-      paging: true, // Aktifkan paginasi
-      searching: true, // Aktifkan pencarian
-      lengthChange: false, // Sembunyikan dropdown "Show [X] entries"
-      pageLength: 5, // Set panjang halaman default menjadi 5 entri per halaman
-      // Konfigurasi DataTables lainnya
-    });
-
-    return () => {
-      // Hapus DataTables saat komponen unmount
-      dataTable.destroy();
-    };
+    getAllDataDosen();
   }, []);
 
-  const [data, setData] = useState([
-    {
-      key: '1',
-      nip: '197312271999031003',
-      nama: 'Ade Chandra Nugraha, S.Si., M.T.',
-      id: 'AD',
-      kode: 'KO001N',
-      email: 'chandra@jtk.polban.ac.id',
-      status: 'Bukan Dosen Wali',
-      kelas: null,
-      prodi: null,
-      image: avatar2,
-    },
-    {
-      key: '2',
-      nip: '198903252019032023',
-      nama: 'Sri Ratna Wulan, S.Pd., M.T.',
-      id: 'SW',
-      kode: 'KO076N',
-      email: 'sri.ratna@jtk.polban.ac.id',
-      status: 'Dosen Wali',
-      kelas: '2A',
-      prodi: 'D3 Teknik Informatika',
-      image: avatar1,
-    },
-  ]);
-
-  const [form, setForm] = useState({}); // Form data
-  // const [detailItem, setDetailItem] = useState(null); // To display details
-  const [editIndex, setEditIndex] = useState(-1); // Index of the data being edited
-
-  // Function to add new data
-  const tambahData = () => {
-    setData([...data, form]);
-    setForm({});
+  const getAllDataDosen = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/data-mahasiswa/students/');
+      setData(response.data.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
   };
 
   // Function to delete data
-  const hapusData = (index) => {
-    const newData = [...data];
-    newData.splice(index, 1);
-    setData(newData);
+  const hapusData = async (id) => {
+    const confirmation = window.confirm('Anda yakin ingin menghapus data ini?');
 
-    // Setelah menghapus data, perlu memeriksa apakah data yang dihapus adalah data yang sedang diedit
-    // Jika ya, kita perlu menghentikan mode pengeditan.
-    if (editIndex === index) {
-      setEditIndex(-1);
-      setForm({});
+    if (confirmation) {
+      try {
+        await axios.delete(`http://localhost:3000/data-dosen/delete/${id}`);
+        const newData = data.filter(item => item.id !== id);
+        setData(newData);
+      } catch (error) {
+        console.error('Error deleting data:', error);
+      }
     }
   };
+
+  const handleSort = (criteria) => {
+    if (criteria === sortBy) {
+      setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    } else {
+      setSortBy(criteria);
+      setSortOrder('asc');
+    }
+  };
+
+  const sortedData = [...data].sort((a, b) => {
+    const order = sortOrder === 'asc' ? 1 : -1;
+
+    if (sortBy === 'Nama') {
+      return order * a.Nama.localeCompare(b.Nama);
+    } else if (sortBy === 'NIM ') {
+      return order * a.NIM.localeCompare(b.NIM);;
+    } else if (sortBy === 'ID_Kelas') {
+      return order * a.Kelas.localeCompare(b.Kelas);
+    }
+  });
 
   // JSX for the header section
   const headerSection = (
     <div className="font-header table-font">
       <div>
-        <h2>Data Dosen</h2>
+        <h2>Data Mahasiswa</h2>
       </div>
     </div>
   );
 
-  // JSX for the container and table-box
-  const mainSection = (
-    <div className="container">
-      <div className="table-box">
-        <CButton href={`/#/tambahDosen/`} onClick={tambahData} className="btn-tambah table-font">
-            + Tambah Data
-        </CButton>
-        <button className="btn-eksport table-font">Eksport</button>
-        <button className="btn-impor table-font">Import</button>
-        <table ref={tableRef} className="tabel">
-          <thead>
-            <tr>
-              <th className="header-cell rata table-font">Nomor</th>
-              <th className="header-cell rata table-font">NIM</th>
-              <th className="header-cell rata table-font">Nama Lengkap</th>
-              <th className="header-cell rata table-font">Kelas</th>
-              <th className="header-cell rata table-font">Aksi</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item, index) => (
-              <tr key={index}>
-                <td className="cell rata table-font">{index + 1}</td>
-                <td className="cell rata table-font">{item.kode}</td>
-                <td className="cell rata table-font">{item.id}</td>
-                <td className="cell rata table-font">{item.nip}</td>
-                <td className="cell aksi">
-                  <CButton href={`/#/detailDosen/${item.key}`} className="margin-button" style={{ color: 'black', backgroundColor: 'transparent' }}>
-                    <CIcon icon={cilInfo} />
-                  </CButton>
-                  <CButton href={`/editDosen/${item.key}`} style={{ color: 'black', backgroundColor: 'transparent' }} >
-                    <CIcon icon={cilPencil} />
-                  </CButton>
-                  <button style={{ backgroundColor: 'transparent' }} onClick={() => hapusData(index)}>
-                    <CIcon icon={cilTrash} />
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+  const pageNumbers = Math.ceil(sortedData.length / itemsPerPage);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentData = sortedData.slice(indexOfFirstItem, indexOfLastItem);
+
+  const handleNextPage = () => {
+    if (currentPage < pageNumbers) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
   return (
     <>
       {headerSection}
-      {mainSection}
+      <div className="container">
+        <div className="table-box">
+          <CButton href={`/#/tu/mahasiswa/tambah/`} className="btn-tambah table-font">
+            + Tambah Data
+          </CButton>
+          <div className="search-input-container">
+            <input
+              type="text"
+              placeholder="Cari..."
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+              className="search-input"
+            />
+            <CIcon icon={cilSearch} className="search-icon" />
+          </div>
+          <table className="tabel">
+            <thead>
+              <tr>
+                <th className="header-cell rata table-font">Nomor</th>
+                <th className="header-cell rata table-font">
+                  <div onClick={() => handleSort('NIM')}>
+                    NIM
+                    <span className="sort-icon">
+                      {sortBy === 'NIM' && sortOrder === 'asc' ? <CIcon icon={cilArrowTop} /> : <CIcon icon={cilArrowBottom} />}
+                    </span>
+                  </div>
+                </th>
+                <th className="header-cell rata table-font">
+                  <div onClick={() => handleSort('Nama')}>
+                    Nama Lengkap
+                    <span className="sort-icon">
+                      {sortBy === 'Nama' && sortOrder === 'asc' ? <CIcon icon={cilArrowTop} /> : <CIcon icon={cilArrowBottom} />}
+                    </span>
+                  </div>
+                </th>
+                <th className="header-cell rata table-font">
+                  <div onClick={() => handleSort('ID_Kelas')}>
+                    Kelas
+                    <span className="sort-icon">
+                      {sortBy === 'ID_Kelas' && sortOrder === 'asc' ? <CIcon icon={cilArrowTop} /> : <CIcon icon={cilArrowBottom} />}
+                    </span>
+                  </div>
+                </th>
+
+                <th className="header-cell rata table-font">Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentData.map((item, index) => (
+                <tr key={item.id}>
+                  <td className="cell rata table-font">{index + 1 + (currentPage - 1) * itemsPerPage}</td>
+                  <td className="cell rata table-font">{item.NIM}</td>
+                  <td className="cell rata table-font">{item.Nama}</td>
+                  <td className="cell rata table-font">{item.ID_Kelas}</td>
+
+                  <td className="cell aksi">
+                    <CButton href={`/#/tu/mahasiswa/detail/${item.id}`} className="margin-button" style={{ color: 'black', backgroundColor: 'transparent' }}>
+                      <CIcon icon={cilInfo} />
+                    </CButton>
+                    <CButton href={`/#/tu/mahasiswa/edit/${item.id}`} style={{ color: 'black', backgroundColor: 'transparent' }}>
+                      <CIcon icon={cilPencil} />
+                    </CButton>
+                    <button style={{ backgroundColor: 'transparent' }} onClick={() => hapusData(item.id)}>
+                      <CIcon icon={cilTrash} />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="pagination">
+            <button
+              className="btn-pagination"
+              onClick={handlePreviousPage}
+              disabled={currentPage === 1}
+            >
+              {'<'}
+            </button>
+            {Array.from({ length: pageNumbers }, (_, i) => {
+              const pageNumber = i + 1;
+              const isActive = pageNumber === currentPage;
+
+              return (
+                <button
+                  key={i}
+                  className={`btn-pagination ${isActive ? 'active' : ''}`}
+                  onClick={() => setCurrentPage(pageNumber)}
+                >
+                  {pageNumber}
+                </button>
+              );
+            })}
+
+            <button
+              className="btn-pagination"
+              onClick={handleNextPage}
+              disabled={currentPage === pageNumbers}
+            >
+              {'>'}
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
