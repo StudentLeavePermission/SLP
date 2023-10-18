0104659fd13a5cfa364344de1d68fa11031d8209
auliaziizah <auliaziizah@gmail.com>
2023-10-18 09:23:17 +0700

    Update Landing Page

--- react/src/views/pages/landingPage/LandingPage.js
+++ react/src/views/pages/landingPage/LandingPage.js
@@ -3,9 +3,7 @@ import {
   CButton,
   CCard,
   CCardBody,
-  CCol,
   CContainer,
-  CRow,
 } from '@coreui/react';
 import CIcon from '@coreui/icons-react';
 import { cilPhone, cilEnvelopeOpen } from '@coreui/icons';
@@ -36,7 +34,7 @@ const LandingPage = () => {
   }, []);
 
   return (
-    <div>
+    <div className='all'>
         <header className="fixed-bar">
             <nav ref={navRef}>
             <button onClick={() => scrollToSection(homeRef)}>Home</button>
@@ -53,11 +51,11 @@ const LandingPage = () => {
         </header>
       
         <div ref={homeRef} className="landing-page">
-            <CContainer className="d-flex justify-content-center position-relative">
+            <CContainer className="home-container">
                 <img src={Lingkaran} alt="Lingkaran" className="landing-image1" />
                 <img src={Student} alt="Student" className="landing-image2" />
                 <div className="landing-content">
-                    <h1 style={{ fontWeight: 'bold' }}>STUDENT LEAVE PERMISION</h1>
+                    <h1 style={{ fontWeight: 'bold' }}>STUDENT LEAVE PERMISSION</h1>
                     <p style={{ textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>Simplify Absence Notifications for Students</p>
                 </div>
             </CContainer>
@@ -69,7 +67,7 @@ const LandingPage = () => {
             <CCard>
                 <CCardBody>
                 <p className="paragraf">
-                    Student Leave Permission adalah sebuah sebuah aplikasi yang mengelola persuratan
+                    Student Leave Permission adalah sebuah aplikasi yang mengelola persuratan
                     ketidakhadiran mahasiswa selama mengikuti perkuliahan dikarenakan mahasiswa sakit atau
                     ada urusan mendadak, sehingga sulit untuk ditinggalkan. Aplikasi menerima data
                     persuratan ketidakhadiran kuliah dari mahasiswa, kemudian mahasiswa mengajukan
@@ -87,25 +85,23 @@ const LandingPage = () => {
         </div>
 
         <div ref={contactRef} className="page">
-            <CContainer className="contact-cont">
-            <CRow style={{ margin: '80px' }}>
-                <CCol className="d-flex justify-content-center" data-aos="fade-right" data-aos-offset="400" data-aos-easing="ease-in-sine"> 
-                    <CCard className="lingkaran">
+            <div className="contact">
+                <div className="contact-content" data-aos="fade-right" data-aos-offset="400" data-aos-easing="ease-in-sine">
+                    <div className="lingkaran">
                         <CIcon icon={cilPhone} size="xl" />
-                    </CCard>
-                    <CCard className="card-contact"> 087735580889 </CCard>
-                </CCol>
-                <CCol className="d-flex justify-content-center" data-aos="fade-left" data-aos-offset="400" data-aos-easing="ease-in-sine"> 
-                    <CCard className="lingkaran">
+                    </div>
+                    <div className="card-contact">087735580889</div>
+                </div>
+                <div className="contact-content" data-aos="fade-left" data-aos-offset="400" data-aos-easing="ease-in-sine">
+                    <div className="lingkaran">
                         <CIcon icon={cilEnvelopeOpen} size="xl" />
-                    </CCard>
-                    <CCard className="card-contact"> fauza.naylassana.tif22@polban.ac.id </CCard>
-                </CCol>
-            </CRow>
-            </CContainer>
+                    </div>
+                    <div className="card-contact">fauza.naylassana.tif22@polban.ac.id</div>
+                </div>
+            </div>
         </div>
     </div>
   )
 }
 
-export default LandingPage
+export default LandingPage;
