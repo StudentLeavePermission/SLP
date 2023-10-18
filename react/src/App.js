7317b6372324fcd9da87033bd33a170045495def
mdifal <mdifa1565@gmail.com>
2023-10-18 10:26:20 +0700

    push crud mhs

--- react/src/App.js
+++ react/src/App.js
@@ -21,6 +21,9 @@ const FormPengajuan = React.lazy(() => import('./views/pages/formPengajuan/FormP
 const LandingPage = React.lazy(()=> import('./views/pages/landingPage/LandingPage'))
 //CRUD Mahasiswa (TU)
 const EditMahasiswa = React.lazy(() => import('./views/pages/crudMahasiswa/EditMahasiswa'))
+const TabelMahasiswa = React.lazy(() => import('./views/pages/crudMahasiswa/tabelMahasiswa'))
+const DetailMahasiswa = React.lazy(() => import('./views/pages/crudMahasiswa/DetailMahasiswa'))
+
 //CRUD Dosen (TU)
 const CrudDosen = React.lazy(() => import('./views/pages/crudDosen/tabelDosen'))
 const DetailDosen = React.lazy(() => import('./views/pages/crudDosen/DetailDosen'))
@@ -43,9 +46,11 @@ class App extends Component {
             <Route exact path="/register" name="Register Page" element={<Register />} />
             <Route exact path="/404" name="Page 404" element={<Page404 />} />
             <Route exact path="/500" name="Page 500" element={<Page500 />} />
-            <Route exact path="/tu/mahasiswa/edit" name="EditMahasiswa" element={<EditMahasiswa />} />
+            <Route exact path="/tu/mahasiswa/edit/:id" name="EditMahasiswa" element={<EditMahasiswa />} />
+            <Route exact path="/tu/mahasiswa/" name="TabelMahasiswa" element={<TabelMahasiswa />} />
+            <Route exact path="/tu/mahasiswa/detail/:id" name="DetailMahasiswa" element={<DetailMahasiswa />} />
             <Route index name="landing page" element={<LandingPage />} />
-            <Route path="*" name="Home" element={<LayoutSLP />} /> 
+            <Route path="*" name="Home" element={<LayoutSLP />} />
           </Routes>
         </Suspense>
       </HashRouter>
