fc88d7c82cb6a5e5365fffed6eefd11cdd702348
nisrinawafaz <nisrinawafaz@gmail.com>
2023-10-23 23:56:31 +0700

    ganti logo

--- react/src/components/AppSidebar.js
+++ react/src/components/AppSidebar.js
@@ -14,7 +14,7 @@ import 'simplebar/dist/simplebar.min.css'
 
 // sidebar nav config
 import navigation from '../_nav'
-import logo from '../assets/images/logo.png'
+import logo from '../assets/images/logo3.png'
 
 const AppSidebar = () => {
   const dispatch = useDispatch()
