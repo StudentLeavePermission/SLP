cd0fd4a4082070a571ca6fe4837f323f813c6b0d
Fauzanaylassana98 <fauza.naylassana.tif22@polban.ac.id>
2023-11-07 23:30:04 +0700

    menambahkan container tabel pada dashboard admin

--- react/src/views/pages/dashboard/dashboardAdmin/dashboard.js
+++ react/src/views/pages/dashboard/dashboardAdmin/dashboard.js
@@ -12,10 +12,12 @@ const DashboardTU = () => {
 
     useEffect(() => {
       getDataPengajuanIzin();
+      // console.log(jmlPengajuanIzin);
     }, []);
     
     useEffect(() => {
       getDataPengajuanSakit();
+      // console.log(jmlPengajuanSakit);
     }, []);
     
     const getDataPengajuanIzin = async () => {
@@ -36,91 +38,217 @@ const DashboardTU = () => {
       console.log('jml data pengajuan: ', jmlPengajuanSakit);
     }, [jmlPengajuanSakit]);
 
-    const random = () => Math.round(Math.random() * 100)
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(5);
+    
+    const pageNumbers = Math.ceil(jmlPengajuanIzin/ itemsPerPage);
+    
+    const handleNextPage = () => {
+      if (currentPage < pageNumbers) {
+        setCurrentPage(currentPage + 1);
+      }
+    };
+  
+    const handlePreviousPage = () => {
+      if (currentPage > 1) {
+        setCurrentPage(currentPage - 1);
+      }
+    };
+
     return (
       <div className="page-dashboard">
-      <CRow>
-        <CCol>
-          <div className="box-grafik">
-            <div className='box-blue-grafik box-grafik'>
-              <div className='box-white-grafik box-grafik'>
-                <CCard className="mb-4 card-grafik">
-                  <CCardHeader className="text-header-grafik">Jumlah Sakit Mahasiswa</CCardHeader>
-                  <CCardBody>
-                    <CChartBar
-                      data={{
-                        labels: ['July', 'August', 'Sept', 'Okt', 'Nov', 'Des', 'Jan', 'Feb', 'March', 'April', 'May', 'June'],
-                        datasets: [
-                          {
-                            label: 'Jumlah sakit per jam pelajaran',
-                            backgroundColor: '#324567',
-                            data: [
-                              jmlPengajuanSakit[6],
-                              jmlPengajuanSakit[7], 
-                              jmlPengajuanSakit[8], 
-                              jmlPengajuanSakit[9], 
-                              jmlPengajuanSakit[10], 
-                              jmlPengajuanSakit[11], 
-                              jmlPengajuanSakit[0], 
-                              jmlPengajuanSakit[1], 
-                              jmlPengajuanSakit[2], 
-                              jmlPengajuanSakit[3], 
-                              jmlPengajuanSakit[4], 
-                              jmlPengajuanSakit[5]
-                            ],
-                          }
-                        ],
-                      }}
-                      labels="months"
-                      style={{ width: '430px' }}
-                    />
-                  </CCardBody>
-                </CCard>
+        <CRow>
+          <CCol>
+            <div className="box-grafik">
+              <div className='box-blue-grafik box-grafik'>
+                <div className='box-white-grafik box-grafik'>
+                  <CCard className="mb-4 card-grafik">
+                    <CCardHeader className="text-header-grafik">
+                      Jumlah Sakit Mahasiswa
+                    </CCardHeader>
+                    <CCardBody>
+                      <CChartBar
+                        data={{
+                          labels: [
+                            'July', 
+                            'August', 
+                            'Sept', 
+                            'Okt', 
+                            'Nov', 
+                            'Des', 
+                            'Jan', 
+                            'Feb', 
+                            'March', 
+                            'April', 
+                            'May', 
+                            'June'
+                          ],
+
+                          datasets: [
+                            {
+                              label: 'Jumlah sakit per jam pelajaran',
+                              backgroundColor: '#324567',
+                              data: [
+                                jmlPengajuanSakit[6],
+                                jmlPengajuanSakit[7], 
+                                jmlPengajuanSakit[8], 
+                                jmlPengajuanSakit[9], 
+                                jmlPengajuanSakit[10], 
+                                jmlPengajuanSakit[11], 
+                                jmlPengajuanSakit[0], 
+                                jmlPengajuanSakit[1], 
+                                jmlPengajuanSakit[2], 
+                                jmlPengajuanSakit[3], 
+                                jmlPengajuanSakit[4], 
+                                jmlPengajuanSakit[5]
+                              ],
+                            }
+                          ],
+                        }}
+                        labels="months"
+                        style={{ width: '430px' }}
+                      />
+                    </CCardBody>
+                  </CCard>
+                </div>
+              </div>
+            </div>
+          </CCol>
+          <CCol>
+            <div className="box-grafik">
+              <div className='box-blue-grafik box-grafik'>
+                <div className='box-white-grafik box-grafik'>
+                  <CCard className="mb-4 card-grafik">
+                    <CCardHeader className="text-header-grafik">Jumlah Izin Mahasiswa</CCardHeader>
+                    <CCardBody>
+                      <CChartBar
+                        data={{
+                          labels: ['July', 'August', 'Sept', 'Okt', 'Nov', 'Des', 'Jan', 'Feb', 'March', 'April', 'May', 'June'],
+                          datasets: [
+                            {
+                              labels: [
+                                'July', 
+                                'August', 
+                                'Sept', 
+                                'Okt', 
+                                'Nov', 
+                                'Des', 
+                                'Jan', 
+                                'Feb', 
+                                'March', 
+                                'April', 
+                                'May', 
+                                'June'
+                              ],
+                              backgroundColor: '#324567',
+                              data: [
+                                jmlPengajuanIzin[6], 
+                                jmlPengajuanIzin[7],
+                                jmlPengajuanIzin[8], 
+                                jmlPengajuanIzin[9], 
+                                jmlPengajuanIzin[10], 
+                                jmlPengajuanIzin[11], 
+                                jmlPengajuanIzin[0], 
+                                jmlPengajuanIzin[1], 
+                                jmlPengajuanIzin[2], 
+                                jmlPengajuanIzin[3], 
+                                jmlPengajuanIzin[4], 
+                                jmlPengajuanIzin[5]
+                              ],
+                            }
+                          ],
+                        }}
+                        labels="months"
+                        style={{ width: '430px' }}
+                      />
+                    </CCardBody>
+                  </CCard>
+                </div>
+              </div>
+            </div>
+          </CCol>
+        </CRow>
+        <CRow>
+          <div>
+            <div className="containerTabel">
+              <div className="containerTabel box-blue">
+              </div>
+              <div className="containerTabel table-box">
+                <div className="table-font">
+                    <h2>Jadwal Kuliah Hari Ini</h2>
+                </div>
+                <table className="tabel">
+                  <thead>
+                      <tr>
+                      <th className="header-cell rata table-font ukuran-kecil">Nomor</th>
+                      <th className="header-cell rata table-font">
+                          <div>
+                            Kelas
+                          </div>
+                      </th>
+                      <th className="header-cell rata table-font">
+                          <div>
+                            Jumlah Izin
+                          </div>
+                      </th>
+                      <th className="header-cell rata table-font">
+                          <div>
+                            Jumlah Sakit
+                          </div>
+                      </th>
+                      <th className="header-cell rata table-font">
+                          <div>
+                            Aksi
+                          </div>
+                      </th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {/* {currentData.map((item, index) => (
+                          <tr key={index}>
+                              <td className="cell rata table-font">{index +1 + (currentPage - 1) * itemsPerPage}</td>
+                              <td className="cell rata table-font">{item.Mata_Kuliah}</td>
+                              <td className="cell rata table-font">{item.Jam}</td>
+                              <td className="cell rata table-font">{item.Nama_Dosen}</td>
+                          </tr>
+                      ))} */}
+                  </tbody>
+                </table>
+                    <div className="pagination">
+                    <button
+                        className="btn-pagination"
+                        onClick={handlePreviousPage}
+                        disabled={currentPage === 1}
+                    >
+                        {'<'}
+                    </button>
+                    {Array.from({ length: pageNumbers }, (_, i) => {
+                        const pageNumber = i + 1;
+                        const isActive = pageNumber === currentPage;
+        
+                        return (
+                        <button
+                            key={i}
+                            className={`btn-pagination ${isActive ? 'active' : ''}`}
+                            onClick={() => setCurrentPage(pageNumber)}
+                        >
+                            {pageNumber}
+                        </button>
+                        );
+                    })}
+        
+                    <button
+                        className="btn-pagination"
+                        onClick={handleNextPage}
+                        disabled={currentPage === pageNumbers}
+                    >
+                        {'>'}
+                    </button>
+                    </div>
               </div>
             </div>
           </div>
-        </CCol>
-        <CCol>
-          <div className="box-grafik">
-            <div className='box-blue-grafik box-grafik'>
-              <div className='box-white-grafik box-grafik'>
-                <CCard className="mb-4 card-grafik">
-                  <CCardHeader className="text-header-grafik">Jumlah Izin Mahasiswa</CCardHeader>
-                  <CCardBody>
-                    <CChartBar
-                      data={{
-                        labels: ['July', 'August', 'Sept', 'Okt', 'Nov', 'Des', 'Jan', 'Feb', 'March', 'April', 'May', 'June'],
-                        datasets: [
-                          {
-                            label: 'Jumlah izin per jam pelajaran',
-                            backgroundColor: '#324567',
-                            data: [
-                              jmlPengajuanIzin[6], 
-                              jmlPengajuanIzin[7],
-                              jmlPengajuanIzin[8], 
-                              jmlPengajuanIzin[9], 
-                              jmlPengajuanIzin[10], 
-                              jmlPengajuanIzin[11], 
-                              jmlPengajuanIzin[0], 
-                              jmlPengajuanIzin[1], 
-                              jmlPengajuanIzin[2], 
-                              jmlPengajuanIzin[3], 
-                              jmlPengajuanIzin[4], 
-                              jmlPengajuanIzin[5]
-                            ],
-                          }
-                        ],
-                      }}
-                      labels="months"
-                      style={{ width: '430px' }}
-                    />
-                  </CCardBody>
-                </CCard>
-              </div>
-            </div>
-          </div>
-        </CCol>
-      </CRow>
+        </CRow>
       </div>
     )
   }
