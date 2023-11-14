bf02889ea9571a010b155e383db1fbf1f99f3fe7
Fauzanaylassana98 <fauza.naylassana.tif22@polban.ac.id>
2023-11-14 20:42:42 +0700

    memperbaiki nama kelas

--- react/src/views/pages/crudJadwal/TabelCRUD.js
+++ react/src/views/pages/crudJadwal/TabelCRUD.js
@@ -11,35 +11,49 @@ import * as XLSX from 'xlsx';
 
 function TabelCRUD({}) {
   const tableRef = useRef(null);  
+
+  // penampung data untuk table
   const [dataJadwal, setDataJadwal] = useState([]);
 
+  // memanggil method yang akan mengambil data dari database
   useEffect(() => {
     getAllClassSchedules();
   }, []);
 
+  //method untuk mengambil data dari database
   const getAllClassSchedules = async () => {
     try {
       const response = await axios.get('http://localhost:3000/jadwal-kelas/formatted');
       const dataJamPelajaran = response.data.jam_pelajaran; 
+
       console.log(dataJamPelajaran);
+
+      //mengambil data yang diperlukan untuk menggantikan id
       const dataDosen = response.data.dosen;        
       const dataMatkul = response.data.mata_kuliah;
       const dataKelas = response.data.kelas;
+
       console.log(dataKelas);
+      
+      //mengganti data id dengan data asli
       const formattedData = response.data.data.map((item, index) => {
         const JamPelajaran = getJamPelajaran(dataJamPelajaran, item.ID_Jam_Pelajaran_Start)+" - "+ tambahIntervalWaktu(getJamPelajaran(dataJamPelajaran, item.ID_Jam_Pelajaran_End), 50); 
         const namaDosen = getNamaDosen(dataDosen, item.ID_Dosen);
         const namaMatkul = getNamaMatkul(dataMatkul, item.ID_Matkul);
-        // const namaKelas = getNamaKelas(dataKelas, item.ID_Kelas);
+        const namaKelas = getNamaKelas(dataKelas, item.ID_Kelas);
+
+        //mappiing data
         return {
           ...item,
           DT_RowId: `${index + 1}`,
           Jam: JamPelajaran,
           Nama_Dosen: namaDosen,
           Mata_Kuliah: namaMatkul,
-          Nama_Kelas: item.ID_Kelas
+          Nama_Kelas: namaKelas
         };
       });
+
+      //memasukkan data yang sudah terformat ke variabel dataJadwal
       setDataJadwal(formattedData);
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -47,13 +61,20 @@ function TabelCRUD({}) {
   }
 
   function tambahIntervalWaktu(time, intervalMenit) {
+
+    //memisahkan jam, menit, detik
     const [jam, menit, detik] = time.split(':').map(Number);
-  
+    
+    // mengubah jam mejadi menit
     const totalMenit = (jam * 60) + menit;
-  
+    
+    // menghitung menit keseluruhannya
     const totalMenitBaru = totalMenit + intervalMenit;
-  
+    
+    //mengubahnya menjadi jam lagi
     const jamBaru = Math.floor(totalMenitBaru / 60);
+
+    //menghitung sisa menit
     const sisaMenit = totalMenitBaru % 60;
   
     // Format hasil baru sebagai tipe data time (HH:MM:SS)
@@ -64,42 +85,75 @@ function TabelCRUD({}) {
 
   function getJamPelajaran (data, id_jam){
     let i = 0;
+
+    //mencari data dalam sekumpulan data
     while (i < data.length){
       if (data[i].id == id_jam){
         return data[i].Waktu_Mulai;
       }
       i++;
     }
+    
     return "NULL";
   }
 
   function getNamaDosen (data, id_dosen){
     let i = 0;
+    
+    //mencari data dalam sekumpulan data
     while (i < data.length){
       if (data[i].Data_Dosen.id == id_dosen){
         return data[i].Data_Dosen.Nama_Dosen;
       }
       i++;
     }
+
     return "NULL";
   }
 
   function getNamaMatkul (data, id_matkul){
     let i = 0;
+    
+    //mencari data dalam sekumpulan data
     while (i < data.length){
       if (data[i].Data_Mata_Kuliah.id == id_matkul){
         return data[i].Data_Mata_Kuliah.Nama_Mata_Kuliah;
       }
       i++;
     }
+
     return "NULL";
   }
 
   function getNamaKelas (data, id_kelas){
     let i = 0;
+    
+    //mencari data dalam sekumpulan data
     while (i < data.length){
       if (data[i].id == id_kelas){
-        return data[i].Nama_Kelas;
+
+        // Mendapatkan tahun sekarang
+        const currentYear = new Date().getFullYear(); 
+        
+        // mendapatkan bulan sekarang untuk menentukan tingkat mahasiswa
+        const currentMonth = new Date().getMonth();
+
+        let angkaKelas = 0;
+
+        if (currentMonth > 6) {
+          //kalau akhir tahun berarti tambah satu
+          angkaKelas = currentYear - data[i].Tahun_Ajaran + 1;
+        } else {
+          //kalau akhir tahun sesuai
+          angkaKelas = currentYear - data[i].Tahun_Ajaran;
+        }
+
+        const tingkat = angkaKelas.toString();
+
+        //formating data biar jadi contoh 2A-D3
+        const kelasFormatted = tingkat + data[i].Nama_Kelas[0] + "-" + data[i].Nama_Kelas.slice(1);
+
+        return kelasFormatted;
       }
       i++;
     }
@@ -118,9 +172,6 @@ function TabelCRUD({}) {
     });
   }, [dataJadwal]);
 
-  const [form, setForm] = useState({}); // Form data
-  const [detailItem, setDetailItem] = useState(null); // To display details
-  const [editIndex, setEditIndex] = useState(-1); // Index of the data being edited
   const [sortBy, setSortBy] = useState('Nomor');
   const [sortOrder, setSortOrder] = useState('asc');
   const [searchText, setSearchText] = useState('');
@@ -184,8 +235,12 @@ function TabelCRUD({}) {
 
     if (confirmation) {
       try {
+        //mengirim data yang akan dihapus
         await axios.delete(`http://localhost:3000/jadwal-kelas/delete/${id}`);
+
         const newData = dataJadwal.filter((item) => item.id !== id);
+
+        //memperbarui dataJadwal
         setDataJadwal(newData);
       } catch (error) {
         console.error('Error deleting data:', error);
@@ -205,6 +260,7 @@ function TabelCRUD({}) {
   const exportToExcel = () => {
     try {
       if(dataJadwal){
+        //formating data
         const dataToExport = dataJadwal.map(item => ({
           No: item.DT_RowId,
           Hari: item.Hari_Jadwal,
@@ -216,6 +272,7 @@ function TabelCRUD({}) {
 
         const ws = XLSX.utils.json_to_sheet(dataToExport);
 
+        //mengatur lebar kolom dispreadsheet yang akan didownload
         const columnWidths = [
           { wch: 3},    // Lebar kolom pertama (No) 3 karakter
           { wch: 10 },  // Lebar kolom kedua (Hari) 10 karakter
@@ -229,7 +286,9 @@ function TabelCRUD({}) {
         ws['!cols'] = columnWidths;
 
         const wb = XLSX.utils.book_new();
-        XLSX.utils.book_append_sheet(wb, ws, 'DataJadwal'); // Nama sheet dalam file Excel
+
+        // Nama sheet dalam file Excel
+        XLSX.utils.book_append_sheet(wb, ws, 'DataJadwal');
 
         // Simpan file Excel dengan nama tertentu (misalnya: data-jadwal.xlsx)
         XLSX.writeFile(wb, 'data-jadwal.xlsx');
