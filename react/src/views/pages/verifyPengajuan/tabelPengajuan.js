6d3340de19b711146262280e0ba34828aded784b
thisisjackii <jaxsix06@gmail.com>
2023-12-11 08:32:21 +0700

    Count Jumlah Jam Izin Tabel Pengajuan

--- react/src/views/pages/verifyPengajuan/tabelPengajuan.js
+++ react/src/views/pages/verifyPengajuan/tabelPengajuan.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
-import 'datatables.net-dt/css/jquery.dataTables.css'; // Import DataTables CSS
-import $ from 'jquery';
-import 'datatables.net';
-import '../../../scss/style.css';
-import axios from 'axios'
+import '../../../scss/style.css'
+import axios from "axios"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import CIcon from '@coreui/icons-react';
 import { CButton } from '@coreui/react';
 import { cilInfo, cilTrash, cilPencil, cilSearch, cilArrowTop, cilArrowBottom } from '@coreui/icons';
@@ -19,22 +17,40 @@ const TabelPengajuanMahasiswa = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [pengajuanResponse, studentD3Response, studentD4Response] = await Promise.all([
+        const [pengajuanResponse, studentD3Response, studentD4Response, jadwalResponse] = await Promise.all([
           axios.get('http://localhost:3000/data-pengajuan'),
           axios.get('http://localhost:3000/data-mahasiswa/students/all/1'),
-          axios.get('http://localhost:3000/data-mahasiswa/students/all/2')
+          axios.get('http://localhost:3000/data-mahasiswa/students/all/2'),
+          axios.get('http://localhost:3000/jadwal-kelas')
         ]);
 
         const studentD3 = studentD3Response.data.data;
         const studentD4 = studentD4Response.data.data;
         const combinedStudentData = [...studentD3, ...studentD4];
 
+        const jadwalData = jadwalResponse.data.data;
+
+        let id_pengajuan_combined = 1;
+
+        let id_pengajuan_combined_list = [];
+
+        console.log("Pengajuan Response: " + JSON.stringify(pengajuanResponse.data))
+
+        console.log("Jadwal Data: " + JSON.stringify(jadwalData));
+
+        console.log("ITEM.ID LOOP LIKELY STARTS HERE");
+
         const groupedLeaveRequests = pengajuanResponse.data.reduce((groups, item) => {
-          const key = `${item.Jenis}_${item.Keterangan}_${item.Tanggal_Pengajuan}`;
+          const key = `${item.Jenis}_${item.Keterangan}_${item.Tanggal_Pengajuan}_${item.Status_Pengajuan}`;
+
+          console.log('item.id = ' + item.id)
+          if (item.id < id_pengajuan_combined) {
+            id_pengajuan_combined = item.id
+          }
 
           if (!groups[key]) {
             groups[key] = {
-              ID: item.id,
+              ID: id_pengajuan_combined,
               Jenis: item.Jenis_Izin,
               Keterangan: item.Keterangan,
               Tanggal: item.Tanggal_Pengajuan,
@@ -44,39 +60,79 @@ const TabelPengajuanMahasiswa = () => {
             };
           }
 
-          groups[key].JamPelajaran.push(getJumlahJamIzin(pengajuanResponse.data, item.Tanggal_Pengajuan, item.Jenis_Izin, item.Keterangan) + " jam");
+          id_pengajuan_combined_list[id_pengajuan_combined++] = item.id
 
+          groups[key].JamPelajaran.push(getJumlahJamIzin(pengajuanResponse.data, jadwalData, item.ID_Jadwal_Kelas, item.Tanggal_Pengajuan, item.Jenis_Izin, item.Keterangan));
+          console.log("GROUPS JP: " + JSON.stringify(groups[key].JamPelajaran))
           return groups;
         }, {});
 
+        console.log("ITEM.ID LOOP LIKELY ENDS HERE");
+        console.log("LIST OF PENGAJUAN ID (ITEM.ID): " + JSON.stringify(id_pengajuan_combined_list))
+        sessionStorage.setItem('stored_item_id_list', JSON.stringify(id_pengajuan_combined_list));
+
+        Object.values(groupedLeaveRequests).forEach((group) => {
+          const uniqueJamPelajaran = Array.from(new Set(group.JamPelajaran.flat()));
+          group.JamPelajaran = uniqueJamPelajaran.length === 1 ? [uniqueJamPelajaran[0]] : uniqueJamPelajaran;
+        });
+
         const dataPengajuan = Object.values(groupedLeaveRequests);
         setDaftarPengajuan(dataPengajuan);
+        console.log('Data pengajuan final: ' + JSON.stringify(dataPengajuan))
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
 
+    function getJamStart(data, id_jadwal) {
+      let i = 0;
+      console.log("Data length: " + data.length + ", id_jadwal = " + id_jadwal)
+      while (i < data.length) {
+        console.log("Data["+i+"].id = "+data[i].id)
+        if (data[i].id === id_jadwal) {
+          console.log("data[i].ID_Jam_Pelajaran_Start: " + data[i].ID_Jam_Pelajaran_Start)
+          return data[i].ID_Jam_Pelajaran_Start;
+        }
+        i++;
+      }
+      console.log("getJamStart returns null. sry")
+      return "NULL";
+    }
+
+    function getJamEnd(data, id_jadwal) {
+      let i = 0;
+      console.log("Data length: " + data.length + ", id_jadwal = " + id_jadwal)
+      while (i < data.length) {
+        console.log("Data["+i+"].id = "+data[i].id)
+        if (data[i].id === id_jadwal) {
+          console.log("data[i].ID_Jam_Pelajaran_End: " + data[i].ID_Jam_Pelajaran_End)
+          return data[i].ID_Jam_Pelajaran_End;
+        }
+        i++;
+      }
+      console.log("getJamEnd returns null. sry")
+      sessionStorage.setItem('')
+      return "NULL";
+    }
+
+    function getJumlahJamIzin(mahasiswa, jadwal, id_jadwal, tgl, jenis, keterangan) {
+      console.log("mahasiswa: " + JSON.stringify(mahasiswa) + ", jadwal: " + JSON.stringify(jadwal) + ", id_jadwal: " + id_jadwal + ", tgl: " + tgl + ", jenis: " + jenis + ", keterangan: " + keterangan)
+      const response = mahasiswa.filter(item => item.Jenis_Izin === jenis && item.Tanggal_Pengajuan === tgl && item.Keterangan.toLowerCase() === keterangan.toLowerCase());
+      let jumlahJP = 0;
+      console.log("Response: " + JSON.stringify(response))
+      response.forEach(item => {
+        console.log("Filtered Jadwal: "+JSON.stringify(jadwal.filter(item => item.id === id_jadwal))+", ID_Jadwal_Kelas: "+id_jadwal);
+        const jamStart = getJamStart(jadwal.filter(item => item.id === id_jadwal), id_jadwal);
+        const jamEnd = getJamEnd(jadwal.filter(item => item.id === id_jadwal), id_jadwal);
+        jumlahJP += jamEnd - jamStart + 1;
+      });
+      console.log("Jumlah Jam Pelajaran getJumlahJamIzin: " + jumlahJP)
+      return jumlahJP;
+    }
+
+
     fetchData();
   }, []);
-
-  const getJamStart = (data, id_jadwal) => {
-    const item = data.find(item => item.id === id_jadwal);
-    return item ? item.ID_Jam_Pelajaran_Start : "NULL";
-  };
-
-  const getJamEnd = (data, id_jadwal) => {
-    const item = data.find(item => item.id === id_jadwal);
-    return item ? item.ID_Jam_Pelajaran_End : "NULL";
-  };
-
-  const getJumlahJamIzin = (data, tgl, jenis, keterangan) => {
-    const response = data.filter(item => item.Jenis_Izin === jenis && item.Tanggal_Pengajuan === tgl && item.Keterangan.toLowerCase() === keterangan.toLowerCase());
-    let jumlahJP = 0;
-    response.forEach(item => {
-      jumlahJP += getJamEnd(data, item.ID_Jadwal_Kelas) - getJamStart(data, item.ID_Jadwal_Kelas) + 1;
-    });
-    return jumlahJP;
-  };
 
   const handleSort = (criteria) => {
     if (criteria === sortBy) {
@@ -157,121 +213,121 @@ const TabelPengajuanMahasiswa = () => {
           </div>
 
           <table className="tabel">
-                <thead>
-                  <tr>
-                    <th className="header-cell rata table-font">Nomor</th>
-                    <th className="header-cell rata table-font">
-                      <div onClick={() => handleSort('NIM')}>
-                        NIM
-                        <span className="sort-icon">
-                          {sortBy === 'NIM' && sortOrder === 'asc' ? <CIcon icon={cilArrowTop} /> : <CIcon icon={cilArrowBottom} />}
-                        </span>
-                      </div>
-                    </th>
-                    <th className="header-cell rata table-font">
-                      <div onClick={() => handleSort('Nama')}>
-                        Nama
-                        <span className="sort-icon">
-                          {sortBy === 'Nama' && sortOrder === 'asc' ? <CIcon icon={cilArrowTop} /> : <CIcon icon={cilArrowBottom} />}
-                        </span>
-                      </div>
-                    </th>
-                    <th className="header-cell rata table-font">
-                      <div onClick={() => handleSort('Jenis')}>
-                        Jenis
-                        <span className="sort-icon">
-                          {sortBy === 'Jenis' && sortOrder === 'asc' ? <CIcon icon={cilArrowTop} /> : <CIcon icon={cilArrowBottom} />}
-                        </span>
-                      </div>
-                    </th>
-                    <th className="header-cell rata table-font">
-                      <div onClick={() => handleSort('Tanggal')}>
-                        Tanggal Pengajuan
-                        <span className="sort-icon">
-                          {sortBy === 'Tanggal' && sortOrder === 'asc' ? <CIcon icon={cilArrowTop} /> : <CIcon icon={cilArrowBottom} />}
-                        </span>
-                      </div>
-                    </th>
-                    <th className="header-cell rata table-font">
-                      <div onClick={() => handleSort('JP')}>
-                        Jumlah Jam Pelajaran
-                        <span className="sort-icon">
-                          {sortBy === 'JP' && sortOrder === 'asc' ? <CIcon icon={cilArrowTop} /> : <CIcon icon={cilArrowBottom} />}
-                        </span>
-                      </div>
-                    </th>
-                    <th className="header-cell rata table-font">
-                      <div onClick={() => handleSort('Status')}>
-                        Status Pengajuan
-                        <span className="sort-icon">
-                          {sortBy === 'Status' && sortOrder === 'asc' ? <CIcon icon={cilArrowTop} /> : <CIcon icon={cilArrowBottom} />}
-                        </span>
-                      </div>
-                    </th>
-                    <th className="header-cell rata table-font">
-                      <div>
-                        Aksi
-                      </div>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {currentData.map((item, index) => (
-                    <tr key={index}>
-                      <td className="cell rata table-font">{index + 1 + (currentPage - 1) * itemsPerPage}</td>
-                      <td className="cell rata table-font">{item.Mahasiswa.NIM}</td>
-                      <td className="cell rata table-font">{item.Mahasiswa.Nama}</td>
-                      <td className="cell rata table-font">{item.Jenis}</td>
-                      <td className="cell rata table-font">{formatTanggal(item.Tanggal)}</td>
-                      <td className="cell rata table-font">{item.JamPelajaran}</td>
-                      <td className="cell rata table-font">{item.Status}</td>
-                      <td className="cell aksi">
-                        <CButton href={`/#/dosen/verifyPengajuan/${item.ID}`} style={{ backgroundColor: 'transparent', color: 'black' }}>
-                          <CIcon icon={cilInfo} />
-                        </CButton>
-                        <CButton href={``} style={{ backgroundColor: 'transparent', color: 'black' }} >
-                          <CIcon icon={cilPencil} />
-                        </CButton>
-                        <CButton onClick={''} style={{ backgroundColor: 'transparent', color: 'black' }}>
-                          <CIcon icon={cilTrash} />
-                        </CButton>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+            <thead>
+              <tr>
+                <th className="header-cell rata table-font">Nomor</th>
+                <th className="header-cell rata table-font">
+                  <div onClick={() => handleSort('NIM')}>
+                    NIM
+                    <span className="sort-icon">
+                      {sortBy === 'NIM' && sortOrder === 'asc' ? <CIcon icon={cilArrowTop} /> : <CIcon icon={cilArrowBottom} />}
+                    </span>
+                  </div>
+                </th>
+                <th className="header-cell rata table-font">
+                  <div onClick={() => handleSort('Nama')}>
+                    Nama
+                    <span className="sort-icon">
+                      {sortBy === 'Nama' && sortOrder === 'asc' ? <CIcon icon={cilArrowTop} /> : <CIcon icon={cilArrowBottom} />}
+                    </span>
+                  </div>
+                </th>
+                <th className="header-cell rata table-font">
+                  <div onClick={() => handleSort('Jenis')}>
+                    Jenis
+                    <span className="sort-icon">
+                      {sortBy === 'Jenis' && sortOrder === 'asc' ? <CIcon icon={cilArrowTop} /> : <CIcon icon={cilArrowBottom} />}
+                    </span>
+                  </div>
+                </th>
+                <th className="header-cell rata table-font">
+                  <div onClick={() => handleSort('Tanggal')}>
+                    Tanggal Pengajuan
+                    <span className="sort-icon">
+                      {sortBy === 'Tanggal' && sortOrder === 'asc' ? <CIcon icon={cilArrowTop} /> : <CIcon icon={cilArrowBottom} />}
+                    </span>
+                  </div>
+                </th>
+                <th className="header-cell rata table-font">
+                  <div onClick={() => handleSort('JP')}>
+                    Jumlah Jam Pelajaran
+                    <span className="sort-icon">
+                      {sortBy === 'JP' && sortOrder === 'asc' ? <CIcon icon={cilArrowTop} /> : <CIcon icon={cilArrowBottom} />}
+                    </span>
+                  </div>
+                </th>
+                <th className="header-cell rata table-font">
+                  <div onClick={() => handleSort('Status')}>
+                    Status Pengajuan
+                    <span className="sort-icon">
+                      {sortBy === 'Status' && sortOrder === 'asc' ? <CIcon icon={cilArrowTop} /> : <CIcon icon={cilArrowBottom} />}
+                    </span>
+                  </div>
+                </th>
+                <th className="header-cell rata table-font">
+                  <div>
+                    Aksi
+                  </div>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentData.map((item, index) => (
+                <tr key={index}>
+                  <td className="cell rata table-font">{index + 1 + (currentPage - 1) * itemsPerPage}</td>
+                  <td className="cell rata table-font">{item.Mahasiswa.NIM}</td>
+                  <td className="cell rata table-font">{item.Mahasiswa.Nama}</td>
+                  <td className="cell rata table-font">{item.Jenis}</td>
+                  <td className="cell rata table-font">{formatTanggal(item.Tanggal)}</td>
+                  <td className="cell rata table-font">{item.JamPelajaran}</td>
+                  <td className="cell rata table-font">{item.Status}</td>
+                  <td className="cell aksi">
+                    <CButton href={`/#/dosen/verifyPengajuan/${item.ID}`} style={{ backgroundColor: 'transparent', color: 'black' }}>
+                      <CIcon icon={cilInfo} />
+                    </CButton>
+                    <CButton href={``} style={{ backgroundColor: 'transparent', color: 'black' }} >
+                      <CIcon icon={cilPencil} />
+                    </CButton>
+                    <CButton onClick={''} style={{ backgroundColor: 'transparent', color: 'black' }}>
+                      <CIcon icon={cilTrash} />
+                    </CButton>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
 
-              <div className="pagination">
+          <div className="pagination">
+            <button
+              className="btn-pagination"
+              onClick={handlePreviousPage}
+              disabled={currentPage === 1}
+            >
+              {'<'}
+            </button>
+            {Array.from({ length: pageNumbers }, (_, i) => {
+              const pageNumber = i + 1;
+              const isActive = pageNumber === currentPage;
+
+              return (
                 <button
-                  className="btn-pagination"
-                  onClick={handlePreviousPage}
-                  disabled={currentPage === 1}
+                  key={i}
+                  className={`btn-pagination ${isActive ? 'active' : ''}`}
+                  onClick={() => setCurrentPage(pageNumber)}
                 >
-                  {'<'}
+                  {pageNumber}
                 </button>
-                {Array.from({ length: pageNumbers }, (_, i) => {
-                  const pageNumber = i + 1;
-                  const isActive = pageNumber === currentPage;
+              );
+            })}
 
-                  return (
-                    <button
-                      key={i}
-                      className={`btn-pagination ${isActive ? 'active' : ''}`}
-                      onClick={() => setCurrentPage(pageNumber)}
-                    >
-                      {pageNumber}
-                    </button>
-                  );
-                })}
-
-                <button
-                  className="btn-pagination"
-                  onClick={handleNextPage}
-                  disabled={currentPage === pageNumbers}
-                >
-                  {'>'}
-                </button>
-              </div>
+            <button
+              className="btn-pagination"
+              onClick={handleNextPage}
+              disabled={currentPage === pageNumbers}
+            >
+              {'>'}
+            </button>
+          </div>
         </div>
       </div>
     </div>
